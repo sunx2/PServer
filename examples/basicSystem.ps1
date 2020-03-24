ed1ce56c9be733acce7e1143c9aad5bb67a909df
Sun <24259556+sunx2@users.noreply.github.com>
2020-03-24 11:47:23 +0530

    Update basicSystem.ps1

--- examples/basicSystem.ps1
+++ examples/basicSystem.ps1
@@ -1,3 +1,4 @@
+Import-Module -Force "Sun.psm1"   #path to sun.psm1 , ex. C://users/files/sun.psm1
 $server = Server
 $server.start_url("http://127.0.0.1:8000/")
 $myfilelist = ls | ConvertTo-Html  #makes html from your current files
